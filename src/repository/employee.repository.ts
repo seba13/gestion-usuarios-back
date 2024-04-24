4273b4673651dc8e624a4719b4a4d24c81367851
snofamv <fniclous97@gmail.com>
2024-04-24 19:32:59 -0400

    m

--- src/repository/employee.repository.ts
+++ src/repository/employee.repository.ts
@@ -68,43 +68,17 @@ export class EmployeeRepository {
   }
   public async getByRut(rutEmployee: string): Promise<IEmployee> {
     const [row] = await this.promise.query<RowDataPacket[]>(
-      `select
-      per.id_persona as idPersona,
-      em.id_empleado as idEmpleado,
-      per.nombre,
-      per.apat as paterno,
-      per.amat as materno,
-      per.fec_nac as fecNac,
-      CONCAT(per.rut,'-',per.dv) as rut,
-      if(
-          per.sexo = 'm', 'Masculino', 'Femenino'
-      ) as sexo,
-      CASE
-          WHEN detalles.ecv="c" THEN 'Casad@'
-          WHEN detalles.ecv="s" THEN 'Solter@'
-          WHEN detalles.ecv="d" THEN 'Divorciad@'
-          WHEN detalles.ecv="v" THEN 'Viud@'
-          ELSE 'Soltero'
-      END AS estadoCivil,
-      detalles.correo,
-      detalles.calle,
-      detalles.numero,
-      detalles.telefono,
-      detalles.profesion,
-      reg.region_nombre as region,
-      com.comuna_nombre as comuna,
-      em.fec_ingreso as fecIngreso,
-      em.fec_despido as fecDespido,
-      est.estado
-  from
-      personas per
-       join personas_detalles dp on dp.id_persona = per.id_persona
-       join empleados em on em.id_persona = per.id_persona
-       join detalles on detalles.id_detalle = dp.id_detalle
-       join regiones reg on reg.region_id = detalles.region
-      join comunas com on com.comuna_id = detalles.comuna
-      join estados est on est.id_estado = em.estado_empleado
-      where per.rut=?;`,
+      `select em.id_empleado as idEmpleado, p.nombre, p.apat as paterno, p.amat as materno, p.fec_nac as fecNac, p.rut as rut, p.dv as dv, 
+      p.sexo,detalles.ecv as 'estadoCivil', detalles.correo, detalles.calle, detalles.numero, detalles.telefono, 
+      detalles.profesion, reg.region_nombre as region, com.comuna_nombre as comuna, em.fec_ingreso as fecIngreso, em.fec_despido as fecDespido, est.estado 
+      from personas_detalles pd
+      join personas p on p.id_persona=pd.id_persona
+      join empleados em on em.id_persona=pd.id_persona
+      join estados est on est.id_estado=em.estado_empleado
+      join detalles on detalles.id_detalle=pd.id_detalle
+      join regiones reg on reg.region_id=detalles.region
+      join comunas com on com.comuna_id=detalles.comuna
+      where em.id_empleado=?;`,
       [rutEmployee]
     );
 
