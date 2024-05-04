975cf540cdd75cad4b96546cbcb3134c9aea821f
sebastian <sebastianalexis869@gmail.com>
2024-05-03 21:25:14 -0400

    test

--- src/utils/server.utils.ts
+++ src/utils/server.utils.ts
@@ -59,8 +59,8 @@ export class ServerResponse {
     res.cookie('cookie-token', data, {
       maxAge: 1200000, // Tiempo de vida de la cookie en milisegundos (aquí, 15 minutos)
       // httpOnly: true, // La cookie solo es accesible en el servidor
-      sameSite: 'None',
-      secure: true,
+      sameSite: 'Lax',
+      secure: false,
     });
     console.log('COOKIE CREADA.');
   }
